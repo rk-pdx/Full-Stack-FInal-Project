09974ba56c8b670a9137d1f5f3cd709badb60114
CristianTS599 <cristiant.salazar@gmail.com>
2022-11-15 07:50:45 -0800

    Implements a Test Case for a new DB router

---
+++ server/src/routers/dbRouter.js
@@ -0,0 +1,29 @@
+const express = require('express');
+//const debug = require('debug')('app:dbRouter');
+const { MongoClient } = require('mongodb');
+const testData = require('../TestData/sessions.json');
+
+const uri =
+  'mongodb+srv://FullStack22:Fall22FullStack@cluster0.tz5vubl.mongodb.net/?retryWrites=true&w=majority';
+
+const dbRouter = express.Router();
+
+dbRouter.route('/').get((req, res) => {
+  const dbName = 'cluster0';
+
+  (async function mongo() {
+    let client;
+
+    try {
+      client = await MongoClient.connect(uri);
+      console.log('Database Connected!');
+      const db = client.db(dbName);
+      const response = await db.collection('sessions').insertMany(testData);
+      res.json(response);
+    } catch (error) {
+      console.log(error.stack);
+    }
+  })();
+});
+
+module.exports = dbRouter;
--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const dbRouter = require('../src/routers/dbRouter');
 
 const app = express();
 
 app.use(cors());
-
+app.use('/db', dbRouter);
 
 app.get('/', (req, res) => {
-    res.send('Welcome page');
+  res.send('Welcome page');
 });
-
 
 app.get('/api', (req, res) => {
-    res.json({"posts":["one", "two", "three"]});
+  res.json({ posts: ['one', 'two', 'three'] });
 });
 
-
-app.listen(5001, () => { console.log("Server started on port 5001") });
+app.listen(5001, () => {
+  console.log('Server started on port 5001');
+});
