c71bc1cf1f6e3103be34a6e1ff51e6de80441fb1
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2022-11-29 10:46:25 -0800

    Styling
    -I changed some of the layout of the pages.
    -The navbar and posting button will now scroll with the page as you go down.
    -I changed the color of the footer and the navbar links/buttons to reflect a darker scheme.
    -Fixed some errors for using for instead of htmlFor in the forms. React can't use for because it is a reserved word so it has to be htmlFor.
    -I noticed the dashboard can be slow to render along with the login at times, but don't know what to do about that.

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,8 +1,56 @@
+import { useState } from 'react';
 import '../contactAbout.css';
+
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(
+      'This feature is not ready to take messages yet. Thank you for your patience.'
+    );
+  };
   return (
-    <div className='container-contact'>
-      <p>Contact</p>
+    <div className='container-wrap'>
+      <div className='user-container'>
+        <h1>Contact</h1>
+        <form className='contact-form' onSubmit={handleSubmit}>
+          <label htmlFor='name'>Your name:</label>
+          <input
+            type='text'
+            name='name'
+            required
+            onChange={(e) => setName(e.target.value.replaceAll(' ', ''))}
+            className='contact-input'
+            placeholder='Your name'
+          />
+          <label htmlFor='email'>Your email:</label>
+          <input
+            type='email'
+            name='email'
+            required
+            onChange={(e) => setEmail(e.target.value.replaceAll(' ', ''))}
+            className='contact-input'
+            placeholder='Your email'
+          />
+          <>
+            <label htmlFor='message'>Your message:</label>
+            <textarea
+              className='contact-input contact-textarea'
+              rows={10}
+              placeholder='Your message'
+              name='message'
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            ></textarea>
+          </>
+          <button type='submit' className='signup-button contact-button'>
+            Send a message
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
